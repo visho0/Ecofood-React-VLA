4a51675ed214657fba3c78d91c219f50968e05bf
Amaro Osses <killsxe123@gmail.com>
2025-05-19 16:36:28 -0400

    Actualizacion Home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,9 @@
-import CardProducto from '../components/CardProducto';
-function Home() {
- return (
- <div className="container mt-4">
- <h1>Productos Disponibles</h1>
- <CardProducto nombre="Pan Integral" precio="$500" />
- </div>
- );
-}
-export default Home;
+import { getUserData } from "../services/userService";
+import { useAuth } from "../context/AuthContext";
+useEffect(() => {
+const fetch = async () => {
+const datos = await getUserData(user.uid);
+setUserData(datos);
+};
+if (user) fetch();
+}, [user]);
