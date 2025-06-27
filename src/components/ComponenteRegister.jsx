78cbdb5dbbbad4fc931e98b590b789d621635942
vicho <visho.duran.205@gmail.com>
2025-06-26 23:31:40 -0400

    actualizacion 26-06

--- src/components/ComponenteRegister.jsx
+++ src/components/ComponenteRegister.jsx
@@ -1,24 +1,24 @@
-<!DOCTYPE html>
-    <><html lang="es">
-        <input
-            type="password"
-            name="password"
-            required
-            minLength={6}
-            maxLength={128}
-            placeholder="Contraseña" />
-        <input
-            type="tel"
-            name="phone"
-            pattern="[0-9]{10,15}"
-            minLength={10}
-            maxLength={15}
-            placeholder="Ej. 3001234567" />
-    </html>
-        <input
-            type="tel"
-            name="phone"
-            pattern="[0-9]{10,15}"
-            minLength={10}
-            maxLength={15}
-            placeholder="Ej. 3001234567" /></>
+import React from "react";
+
+const ComponenteRegister = () => (
+  <>
+    <input
+      type="password"
+      name="password"
+      required
+      minLength={6}
+      maxLength={128}
+      placeholder="Contraseña"
+    />
+    <input
+      type="tel"
+      name="phone"
+      pattern="[0-9]{10,15}"
+      minLength={10}
+      maxLength={15}
+      placeholder="Ej. 3001234567"
+    />
+  </>
+);
+
+export default ComponenteRegister;
--- src/pages/cliente/ClienteDashboard.jsx
+++ src/pages/cliente/ClienteDashboard.jsx
@@ -1,3 +1,3 @@
-export default function ClienteDashboard() { [cite, 29]
-  return <h2>Bienvenido cliente</h2>; [cite, 29]
+export default function ClienteDashboard() { 
+  return <h2>Bienvenido cliente</h2>; 
 }
